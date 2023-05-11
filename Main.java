403ceaf5dc82cfc12e2805045a610932b230aaef
wildanapk <wildanbusiness9@gmail.com>
2023-05-11 22:10:00 +0700

    uts

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-class Main {
+import pengguna.Mhs;
+
+public class Main {
     public static void main(String[] args) {
 
+        Mhs mhs = new Mhs("Muhammad Wildan Nur Fadhlullah", "G111190016", 3, "ini_username", "ini_password");
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUser().getUsername());
+        System.out.println("Password: " + mhs.getUser().getPassword());
     }
 }
--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,43 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
+public class Mhs {
+    private String nama;
+    private String nim;
+    private int smt;
+    private User user;
 
+    public Mhs(String nama, String nim, int smt, String username, String password) {
+        this.nama = nama;
+        this.nim = nim;
+        this.smt = smt;
+        this.user = new User(username, password);
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public int getSmt() {
+        return smt;
+    }
+
+    public void setSmt(int smt) {
+        this.smt = smt;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
